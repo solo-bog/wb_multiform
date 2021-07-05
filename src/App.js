736cad2131ed67766625654799b5b5f5c846c3bf
solo-bog <bsoloxa@gmail.com>
2021-07-05 11:04:01 +0300

    add eslint, prettier and linting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
