61d5d27d12d99e210568f328980dc17e4630fb2d
solo-bog <bsoloxa@gmail.com>
2021-07-05 11:52:31 +0300

    create StepForm and Button components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import StepForm from "./components/StepForm";
+
+function App() {
+  return (
+    <div className="App">
+      <StepForm />
+    </div>
+  );
+}
+
+export default App;
